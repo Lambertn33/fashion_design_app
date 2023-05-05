5b186fbabe0286b3c8551427ade7e56537bcf83f
Lambertn33 <lambertgithub@gmail.com>
2023-05-05 17:12:25 +0200

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(title: Text('Fashion design'),)),
+    );
+  }
+}
